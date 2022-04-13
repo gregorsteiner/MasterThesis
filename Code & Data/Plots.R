011b8b512a71e04abc9b611760b791c1d7c9cea0
Gregor Steiner <gregorsteiner7@gmail.com>
2022-04-13 11:00:32 +0200

    Plots

--- Code & Data/Plots.R
+++ Code & Data/Plots.R
@@ -18,6 +18,15 @@ plot_usmap(data = seda.cov[year == 2018 & grade == 3,
   scale_fill_viridis_c(name = "Median Income (2018)")
 
 
+# plot test scores
+plot_usmap(data = seda.comb[year == 2016 & grade == 3 & subject == "mth",
+                                   .(Scores = gcs_mn_all, fips = sedacounty, subject)],
+                  values = "Scores") +
+  scale_fill_viridis_c(name = "Test Scores")
+
+  
+
+
 
 # plot cumulative disasters
 plot_usmap(data = fema.cum, values = "Disasters") +
