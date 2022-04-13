4118a108f5c596352bfbe220549bfd70856a8c34
Gregor Steiner <gregorsteiner7@gmail.com>
2022-04-13 13:53:37 +0200

    Simple model

--- Code & Data/Models.R
+++ Code & Data/Models.R
@@ -1,3 +1,24 @@
 
 
+######## Preliminaries ########
+
+source("AuxFunctions.R")
+library(data.table)
+library(fixest)
+
+
+# read data
+dat <- readRDS("Data.RDS")
+
+
+######## Models ########
+
+# county fixed effects
+model <- feols(gcs_mn_all ~ CumuDisasters | fips, data = dat)
+etable(model)
+
+# check residuals
+resid <- residuals(model)
+qqnorm(resid)
+qqline(resid, lwd = 2, col = 2)
 
