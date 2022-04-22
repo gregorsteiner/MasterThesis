7406d344f44e72ef0a01929f4f0a0b152869c459
Gregor Steiner <gregorsteiner7@gmail.com>
2022-04-22 14:18:32 +0200

    First results

--- Code & Data/Models.R
+++ Code & Data/Models.R
@@ -17,10 +17,20 @@ dat <- readRDS("Data.RDS")
 model <- feols(c(cs_mn_all, cs_mn_wbg, cs_mn_mfg, cs_mn_neg) ~ 
                  DisasterDummy | year + grade + subject,
                data = dat, vcov = "iid")
-etable(model)
+
+# automatically export as tex file
+etable(model, coefstat = "confint",
+       file = "../TeX Files/MainResults.tex", replace = TRUE,
+       label = "MainResults", title = "Results",
+       dict=c(cs_mn_all = "Mean test score", cs_mn_wbg = "White-Black gap",
+              cs_mn_mfg = "Male-Female gap", cs_mn_neg = "Disadvantaged gap"))
+
 
 # check residuals
 resid <- residuals(model)
+
+plot(resid[, 1])
+
 qqnorm(resid)
 qqline(resid, lwd = 2, col = 2)
 
